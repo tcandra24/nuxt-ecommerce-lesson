65c66f16e5a8251923e000195e047352d08e10ce
Tito Candra <tcandra007@gmail.com>
2023-01-01 07:33:14 +0700

    Initial Commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -157,7 +157,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://ecommerce.apptitodev.my.id/',
+    baseURL: 'https://www.ecommerce.apptitodev.my.id/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
